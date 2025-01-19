8bfa7fc5f6a57814bd5715ced43df1dd412a44af
Vasily Tsybenko <vasayxtx@gmail.com>
2025-01-19 22:35:29 +0200

    Add atuin configuration

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 ln -sf ~/dotfiles/zed/settings.json ~/.config/zed/settings.json
 ln -sf ~/dotfiles/zed/keymap.json ~/.config/zed/keymap.json
+
+ln -sf ~/dotfiles/atuin/config.toml ~/.config/atuin/config.toml
