c5e3e5edc013e138b3b24895c96190d85857c699
Vasily Tsybenko <vasayxtx@gmail.com>
2025-01-19 22:32:54 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+ln -sf ~/dotfiles/zed/settings.json ~/.config/zed/settings.json
+ln -sf ~/dotfiles/zed/keymap.json ~/.config/zed/keymap.json
